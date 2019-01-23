3b197852df71c0c5948956a66272423783c86886
Mark Kanko <mkanko66@gmail.com>
2019-01-23 02:05:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require'pry'
+
 def roll_call_dwarves(names)
   names.each_with_index {|item, index| puts "#{index + 1} + #{item}"}
 end
@@ -17,10 +19,8 @@ def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   list.find {|item| item == cheese_types}
-
+binding.pry
     
- 
-  
 end
 
 # .select / .detect / .find
