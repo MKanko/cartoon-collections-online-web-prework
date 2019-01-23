0e9023c489d08f6d0b60dde70752454eea8cac3d
Mark Kanko <mkanko66@gmail.com>
2019-01-23 03:24:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.find {|item| item.include cheese_types}
+  list.find {|item| cheese_types.include[item]}
 end
 
 # look at .select / .detect / .find
